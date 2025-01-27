43009cef0cbaf7bf7cc081d029c3420d02e92006
yesuiii <e.yesui@letumongolia.com>
2025-01-27 15:58:48 +0800

    first commit

---
+++ food.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+from PIL import Image
+# Load the trained model
+@st.cache_resource
+def load_model():
+    learn = load_learner('mongolian_food_classifier.pkl')
+    return learn
+
+def main():
+    st.title("Mongolian Food Classifier")
+    st.write("Upload an image")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        learn = load_model()
+
+        pred, pred_idx, probs = learn.predict(image)
+
+        # Display results
+        st.write(f"### Prediction: {pred}")
+        st.write(f"### Confidence: {probs[pred_idx]:.4f}")
+
+if __name__ == "__main__":
+    main()
