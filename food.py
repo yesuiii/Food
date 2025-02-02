55d058746b352d2ddaef1484225897bd2ac51af9
yesuiii <e.yesui@letumongolia.com>
2025-02-02 14:30:29 +0800

    food.py

--- food.py
+++ food.py
@@ -1,6 +1,7 @@
 import streamlit as st
-#from fastai.vision.all import *
+from fastai.vision.all import *
 from PIL import Image
+
 # Load the trained model
 @st.cache_resource
 def load_model():
@@ -14,7 +15,6 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_container_width=True)
 
